d3e9d9bd77d03cb3fa2ab8f56ffc31eb2a0f17c5
Carlos <carloseduardomenegassi0@gmail.com>
2026-04-12 12:23:22 -0300

    feat: Criei o Menu lateral.

---
+++ FiapGames.Web/Components/Shared/SearchBar.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace FiapGames.Web.Components.Shared;
+
+public partial class SearchBar
+{
+    [Parameter] public string Placeholder { get; set; } = "Buscar jogos...";
+    [Parameter] public EventCallback<string> OnSearch { get; set; }
+
+    private string SearchText { get; set; } = string.Empty;
+
+    private async Task HandleKeyDown(KeyboardEventArgs e)
+    {
+        if (e.Key == "Enter")
+            await OnSearch.InvokeAsync(SearchText);
+    }
+
+    private async Task ClearSearch()
+    {
+        SearchText = string.Empty;
+        await OnSearch.InvokeAsync(SearchText);
+    }
+}
