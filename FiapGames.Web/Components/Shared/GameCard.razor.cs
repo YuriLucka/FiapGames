f3e1109ba90e34a73e1bd4331003951304ad20cd
Carlos <carloseduardomenegassi0@gmail.com>
2026-04-13 18:00:01 -0300

    feat: Finalizado componentes iniciais. Falta conectar na API

---
+++ FiapGames.Web/Components/Shared/GameCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FiapGames.Web.Components.Shared;
+
+public partial class GameCard
+{
+    [Parameter, EditorRequired] public string Title { get; set; } = string.Empty;
+    [Parameter, EditorRequired] public string Price { get; set; } = string.Empty;
+    [Parameter] public string ImageUrl { get; set; } = "/images/placeholder.png";
+}
